e0c1acce53a7d60570933b1c4991c3f5dfc7ae2c
VikramRamanathan <vikramramanathan9@gmail.com>
2022-12-31 10:57:09 +0800

    Added some alignment, commented better, fixed imports

--- src/cpop.py
+++ src/cpop.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.stats import stats
 from sklearn.model_selection import train_test_split 
 from sklearn.linear_model import LinearRegression
 from sklearn import metrics
@@ -16,13 +17,14 @@ def estimateConstantPeriod(timings):
 
     # Its just a simple linear regression on the timings
     x_train, x_test, y_train, y_test = train_test_split(np.arange(len(timings)).reshape((-1, 1)), timings, test_size=0.2, random_state=0)
-    
-    regressor = LinearRegression()  
-    regressor.fit(x_train, y_train) #training the algorithm
+
+    regressor = LinearRegression()
+    regressor.fit(x_train, y_train)  # training the algorithm
 
     # Print the error of estimation
     print("MSE of estimation", metrics.mean_squared_error(y_test, regressor.predict(x_test)))
     return regressor.coef_[0], regressor.intercept_
+
 
 def distance_metric(dist, spans):
     medians = np.median(dist, axis=0)
@@ -30,14 +32,17 @@ def distance_metric(dist, spans):
     return np.sum(distances, axis=0)
 
 
-def period_stupid_search(data, guess):
+def period_stupid_search(data, deltas):
     # Ok this took a lot of tinkering, but it kinda works? I tried smarter searches but they were unreliable
     # It's basically just an iterative grid search
     # TODO: Do more tinkering to see what the general minimum required computation for convergence is
 
     # The initial guess should be the median difference between eclipses
     # I still need to figure out how to handle systems with weird phases for secondary eclipses
-    delta = max(round(guess / 10, 1), 0.1) / guess
+    guess = deltas.median()
+    data = align_data(data, guess / 2)
+
+    change = max(round(guess / 10, 1), 0.1) / guess
     no_probes = 51
     max_dim = len(data)
     data = np.expand_dims(data, 1)
@@ -46,8 +51,8 @@ def period_stupid_search(data, guess):
     bestval = distance_metric(data % guess, guess)
 
     count = 0
-    while count < 100 and delta > 1e-8:
-        probes = np.linspace(guess - guess * delta, guess + guess * delta, no_probes)
+    while count < 100 and change > 1e-8:
+        probes = np.linspace(guess - guess * change, guess + guess * change, no_probes)
         results = np.mod(np.broadcast_to(data, (max_dim, no_probes)), probes)
 
         distances = distance_metric(results, probes)
@@ -58,12 +63,16 @@ def period_stupid_search(data, guess):
             bestval = distances[seed]
 
         if seed < no_probes // 2:
-            guess = guess - delta / 2
+            guess = guess - change / 2
         elif seed > no_probes // 2:
-            guess = guess + delta / 2
-        delta = delta * 3 / 4
+            guess = guess + change / 2
+        change = change * 3 / 4
         count = count + 1
     return best
+
+
+def align_data(data, new_offset):
+    return data - data[0] + new_offset
 
 
 def remove_extremes(arr):
@@ -90,9 +99,9 @@ def getOC(eclipse, author="Vikram"):
     """
     
     if author == "Vikram":
-        period = period_stupid_search(eclipse['time'], eclipse['delta'].median())
+        period = period_stupid_search(eclipse['time'], eclipse['delta'])
         offset = (eclipse['time'] % period).median()
-    elif author == "Yuan Xi":
+    elif author == "Yuan Xi":  # this one is mathematically wrong, do not use unless very sure
         period, offset = estimateConstantPeriod(eclipse['time'])
 
     # return eclipse['time'] - offset - np.arange(len(eclipse['time'])) * period
