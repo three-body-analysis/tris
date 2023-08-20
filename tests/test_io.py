fc1b879e66665d4eaa0c8e1eaa2d8407cca8c8a1
Prannaya <prannayagupta@gmail.com>
2023-08-20 11:03:31 +0800

    feat(tests): add excel and json tests

--- tests/test_io.py
+++ tests/test_io.py
@@ -4,7 +4,7 @@ from glob import glob
 import pandas as pd
 import numpy as np
 
-from tris.io import read_df, read_csv
+from tris.io import read_df, read_csv, read_excel, read_json
 
 import warnings
 
@@ -15,10 +15,6 @@ class DataReadTestCase(unittest.TestCase):
     def test_read_df(self):
         df = pd.read_csv("tests/sample_data/processed.csv")
         df_copy = pd.read_csv("tests/sample_data/processed.csv")
-        df_copy["col0"] = np.linspace(0, 1000, df.shape[0])
-        df_copy[["col1", "col2"]] = np.random.random((df.shape[0], 2))
-
-        df_copy = df_copy[["col0", "time", "col1", "flux", "col2"]]
 
         df_test_full_index = read_df(df, 0, 1)
         self.assertTrue(df.equals(df_test_full_index), "Full Index Read Test (identity) Failed")
@@ -71,15 +67,71 @@ class DataReadTestCase(unittest.TestCase):
         df_test_index_name = read_csv("tests/sample_data/manipulated.csv", 1, "flux")
         self.assertTrue(df.equals(df_test_index_name), "Index, Name Read Test (manipulated) Failed")
 
-    # def test_all(self):
-    #     all_systems = glob("../data/combined/*.fits")
-    #     for filename in tqdm(all_systems):
-    #         system = filename.split("\\")[-1].split('.')[0]
-    #         df = read(filename)
-    #         self.assertEqual(
-    #             tuple(df.columns), ("time", "flux"),
-    #             f"Columns don't match for {system}: Expected ('time', 'flux'), found {tuple(df.columns)}"
-    #         )
+    def test_excel(self):
+        df = pd.read_csv("tests/sample_data/processed.csv")
+
+        df_test_index_index_index = read_excel("tests/sample_data/processed.xlsx", 0, 1, 0)
+        self.assertTrue(df.equals(df_test_index_index_index), "Index-Index-Index Read Test (identity) Failed")
+
+        df_test_index_index_name = read_excel("tests/sample_data/processed.xlsx", 0, 1, "processed")
+        self.assertTrue(df.equals(df_test_index_index_name), "Index-Index-Name Read Test (identity) Failed")
+
+        df_test_index_name_index = read_excel("tests/sample_data/processed.xlsx", 0, "flux", 0)
+        self.assertTrue(df.equals(df_test_index_name_index), "Index-Name-Index Read Test (identity) Failed")
+
+        df_test_index_name_name = read_excel("tests/sample_data/processed.xlsx", 0, "flux", "processed")
+        self.assertTrue(df.equals(df_test_index_name_name), "Index-Name-Name Read Test (identity) Failed")
+
+        df_test_name_index_index = read_excel("tests/sample_data/processed.xlsx", "time", 1, 0)
+        self.assertTrue(df.equals(df_test_name_index_index), "Name-Index-Index Read Test (identity) Failed")
+
+        df_test_name_index_name = read_excel("tests/sample_data/processed.xlsx", "time", 1, "processed")
+        self.assertTrue(df.equals(df_test_name_index_name), "Name-Index-Name Read Test (identity) Failed")
+
+        df_test_name_name_index = read_excel("tests/sample_data/processed.xlsx", "time", "flux", 0)
+        self.assertTrue(df.equals(df_test_name_name_index), "Name-Name-Index Read Test (identity) Failed")
+
+        df_test_name_name_name = read_excel("tests/sample_data/processed.xlsx", "time", "flux", "processed")
+        self.assertTrue(df.equals(df_test_name_name_name), "Name-Name-Name Read Test (identity) Failed")
+
+        df_test_index_index_index = read_excel("tests/sample_data/manipulated.xlsx", 1, 3, 0)
+        self.assertTrue(df.equals(df_test_index_index_index), "Index-Index-Index Read Test (identity) Failed")
+
+        df_test_index_index_name = read_excel("tests/sample_data/manipulated.xlsx", 1, 3, "manipulated")
+        self.assertTrue(df.equals(df_test_index_index_name), "Index-Index-Name Read Test (identity) Failed")
+
+        df_test_index_name_index = read_excel("tests/sample_data/manipulated.xlsx", 1, "flux", 0)
+        self.assertTrue(df.equals(df_test_index_name_index), "Index-Name-Index Read Test (identity) Failed")
+
+        df_test_index_name_name = read_excel("tests/sample_data/manipulated.xlsx", 1, "flux", "manipulated")
+        self.assertTrue(df.equals(df_test_index_name_name), "Index-Name-Name Read Test (identity) Failed")
+
+        df_test_name_index_index = read_excel("tests/sample_data/manipulated.xlsx", "time", 3, 0)
+        self.assertTrue(df.equals(df_test_name_index_index), "Name-Index-Index Read Test (identity) Failed")
+
+        df_test_name_index_name = read_excel("tests/sample_data/manipulated.xlsx", "time", 3, "manipulated")
+        self.assertTrue(df.equals(df_test_name_index_name), "Name-Index-Name Read Test (identity) Failed")
+
+        df_test_name_name_index = read_excel("tests/sample_data/manipulated.xlsx", "time", "flux", 0)
+        self.assertTrue(df.equals(df_test_name_name_index), "Name-Name-Index Read Test (identity) Failed")
+
+        df_test_name_name_name = read_excel("tests/sample_data/manipulated.xlsx", "time", "flux", "manipulated")
+        self.assertTrue(df.equals(df_test_name_name_name), "Name-Name-Name Read Test (identity) Failed")
+
+    def test_json(self):
+        df = pd.read_csv("tests/sample_data/processed.csv")
+
+        df_test_full_index = read_json("tests/sample_data/processed.json", 0, 1)
+        self.assertTrue(df.equals(df_test_full_index), "Full Index Read Test (identity) Failed")
+
+        df_test_full_name = read_json("tests/sample_data/processed.json", "time", "flux")
+        self.assertTrue(df.equals(df_test_full_name), "Full Name Read Test (identity) Failed")
+
+        df_test_name_index = read_json("tests/sample_data/processed.json", "time", 1)
+        self.assertTrue(df.equals(df_test_name_index), "Name, Index Read Test (identity) Failed")
+
+        df_test_index_name = read_json("tests/sample_data/processed.json", 0, "flux")
+        self.assertTrue(df.equals(df_test_index_name), "Index, Name Read Test (identity) Failed")
 
 
 if __name__ == '__main__':
